ff3584b5388d5a66cd5fddfed751960d8c06cd6f
= <=>
2024-03-26 22:57:38 +0000

    Not working aaaaa

--- LLamaTricker/src/main.rs
+++ LLamaTricker/src/main.rs
@@ -1,5 +1,7 @@
 use std::process::{Command, Stdio};
-use std::io::{self, Read, Write};
+use std::io::{ Read, Write};
+use std::thread::sleep;
+use std::time;
 
 fn main(){
     println!("LLamaTricker: The LLama2 convincer!");
@@ -21,12 +23,19 @@ fn main(){
         stdin.write_all(input_text.as_bytes()).expect("Failed to write to stdin");
     }
 
-    let status = c1.wait().expect("Failed to wait for Ollama process");
+    while c1.stdout.is_none() {} //While there is no output
+
+    let delay = time::Duration::from_secs(20);
+    sleep(delay); //Once output detected, wait 10 seconds to allow for prompt completion
 
     let mut output_text = String::new();
+    print!("A");
     if let Some(mut stdout) = c1.stdout.take() {
+        print!("B");
         let mut buffer = [0; 1]; // Read one byte at a time
+        print!("C");
         loop {
+            print!("D");
             match stdout.read(&mut buffer) {
                 Ok(0) => break, // End of output
                 Ok(_) => output_text.push(buffer[0] as char),
