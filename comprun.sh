d467a27c177d74606d2fa432218e48c8807dacad
= <=>
2024-03-27 20:39:37 +0000

    Removed the c project and made rust the root

--- comprun.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-gcc mlidewiwitdane.c
-./a.out
---
+++ target/debug/deps/LLamaTricker-e8bdb77cf318af46.d
@@ -0,0 +1,5 @@
+/home/ubuntu/SCN/LLamaTricker/target/debug/deps/libLLamaTricker-e8bdb77cf318af46.rmeta: src/main.rs
+
+/home/ubuntu/SCN/LLamaTricker/target/debug/deps/LLamaTricker-e8bdb77cf318af46.d: src/main.rs
+
+src/main.rs:
